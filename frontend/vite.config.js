6d3ef4bce1a5e9d76beebc414b8ccf93eaf2c770
jflievanoh-ui <jflievanoh@gmail.com>
2025-08-19 12:01:43 -0500

    New

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://videolink-backend.onrender.com',
-      '/ws': {
-        target: 'wss://videolink-backend.onrender.com',
+      "/api": "https://videolink-backend.onrender.com",
+      "/ws": {
+        target: "wss://videolink-backend.onrender.com",
         ws: true,
       },
     },
+  },
+  build: {
+    outDir: "dist", // 👈 required for Render deployment
   },
 });
